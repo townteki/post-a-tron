d6503441604df2a8025d3e21222c67c9c85f2c86
justintwynn <justintwynn@gmail.com>
2018-05-08 17:55:04 -0400

    Remove manamoji middleware

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Discord = require("discord.js");
 const Servo = require('./lib/bot');
 
 if (!process.env.DISCORD_TOKEN) {
-  console.log('Error: Specify SLACK_TOKEN in environment');
+  console.log('Error: Specify DISCORD_TOKEN in environment');
   process.exit(1);
 }
 
