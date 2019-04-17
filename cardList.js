28ff71fc6654f982015e61fecdb759705abc7faf
Justin Wynn <justintwynn@gmail.com>
2019-04-16 21:48:44 -0400

    Retrieve card data from api

--- cardList.js
+++ cardList.js
@@ -11,11 +11,7 @@ var options = {
     json: true // Automatically parses the JSON string in the response
 };
 
-module.exports = request(options)
-  .then(function (result) {
-      console.log(result);
-      return result;
-  })
-  .catch(function (err) {
-      throw err;
-});
+module.exports = request(options);
+
+
+
