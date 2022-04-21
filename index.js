aa5d8c2a349036709c9320813d85877232e39e86
BriscoLargg <spamman5r@gmail.com>
2022-04-21 17:50:26 -0400

    In progress TextResponse enhancements

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var Discord = require("discord.js");
 const Servo = require('./lib/bot');
 
-if (!process.env.DISCORD_TOKEN) {
-  console.log('Error: Specify DISCORD_TOKEN in environment');
-  process.exit(1);
-}
+// if (!process.env.DISCORD_TOKEN) {
+//   console.log('Error: Specify DISCORD_TOKEN in environment');
+//   process.exit(1);
+// }
 
-new Servo(process.env.DISCORD_TOKEN);
+new Servo("MzU1MDc2MDQxNjgyOTc2NzY5.DJHh4Q.vURr1GusnC4nRZ4eCHCmoF0T1zM");
