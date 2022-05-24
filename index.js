f4e32fa503e7da979558aad45a6d534c13e04bef
BriscoLargg <spamman5r@gmail.com>
2022-05-24 12:52:30 -0400

    Reset discord token and replace hack in index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var Discord = require("discord.js");
 const Servo = require('./lib/bot');
 
-// if (!process.env.DISCORD_TOKEN) {
-//   console.log('Error: Specify DISCORD_TOKEN in environment');
-//   process.exit(1);
-// }
-
-new Servo("MzU1MDc2MDQxNjgyOTc2NzY5.DJHh4Q.vURr1GusnC4nRZ4eCHCmoF0T1zM");
+if (!process.env.DISCORD_TOKEN) {
+  console.log('Error: Specify DISCORD_TOKEN in environment');
+  process.exit(1);
+}
