2afd1435eb0c045f6d000f8a530ce415f6245df0
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-07-23 09:27:48 -0700

    reorg import script to use async/await syntax and extract data fetcher into utility method for reuse.

--- scripts/import.js
+++ scripts/import.js
@@ -12,97 +12,110 @@ const exportFile = path.join(__dirname, '../data/cards.json');
  * @return {Number}
  */
 function sortCardCodes(code1, code2) {
-    return parseInt(code2, 10) - parseInt(code1, 10);
+  return parseInt(code2, 10) - parseInt(code1, 10);
 }
 
 const cardTextReplacement = [
-    // Markup to Markdown
-    [/<b>/g, "**"],
-    [/<\/b>/g, "**"],
-    [/<i>/g, "***"],
-    [/<\/i>/g, "***"],
-    // Line Breaks to double line breaks, otherwise markdown will just eat the break.
-    [/\n/g, '\n\n']
+  // Markup to Markdown
+  [/<b>/g, "**"],
+  [/<\/b>/g, "**"],
+  [/<i>/g, "***"],
+  [/<\/i>/g, "***"],
+  // Line Breaks to double line breaks, otherwise markdown will just eat the break.
+  [/\n/g, '\n\n']
 ];
 
-https.get('https://dtdb.co/api/cards/', (res) => {
+async function getDataFromAPI(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      const { statusCode } = res;
+      const contentType = res.headers['content-type'];
 
-    const { statusCode } = res;
-    const contentType = res.headers['content-type'];
-
-    let error;
-    if (statusCode !== 200) {
+      let error;
+      if (statusCode !== 200) {
         error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
-    } else if (!/^application\/json/.test(contentType)) {
+      } else if (!/^application\/json/.test(contentType)) {
         error = new Error('Invalid content-type.\n' +
-            `Expected application/json but received ${contentType}`);
-    }
+          `Expected application/json but received ${contentType}`);
+      }
 
-    if (error) {
+      if (error) {
         console.error(error.message);
-        res.resume();
-        return;
+        reject(error.message);
+      }
+      res.setEncoding('utf8');
+      let rawData = '';
+      res.on('data', (chunk) => { rawData += chunk; });
+      res.on('end', () => {
+        try {
+          const json = JSON.parse(rawData);
+          resolve(json);
+        } catch (e) {
+          console.error(e.message);
+          reject(e.message);
+        }
+      });
+      res.on('error', (e) => {
+        console.error(e);
+        reject(e.message);
+      });
+    })
+  });
+}
+
+async function importCards() {
+
+  const cards = await getDataFromAPI('https://dtdb.co/api/cards/');
+
+  // output data structure
+  const data = {
+    cards: {},
+    indices: {
+      packs: {},
+      names: {}
+    },
+  }
+
+  cards.forEach(card => {
+    // ignore alt-arts
+    if (card.pack_code === 'alt') {
+      return;
+    }
+    // massage card text and flavor-text.
+    if (card.text) {
+      cardTextReplacement.forEach(searchReplace => {
+        card.text = card.text.replace(searchReplace[0], searchReplace[1]);
+      });
     }
 
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', (chunk) => { rawData += chunk; });
-    res.on('end', () => {
-        let cards;
-        try {
-            cards = JSON.parse(rawData);
-        } catch (e) {
-            console.error(e.message);
-        }
+    // store card by code.
+    data.cards[card.code] = card;
+    const packKey = makeKeyFromPackCode(card.pack_code);
+    const nameKey = makeKeyFromName(card.title);
 
-        // output data structure
-        const data = {
-            cards: {},
-            indices: {
-                packs: {},
-                names: {}
-            },
-        }
+    // index cards by pack
+    if (! data.indices.packs.hasOwnProperty(packKey)) {
+      data.indices.packs[packKey] = {};
+    }
+    data.indices.packs[packKey][nameKey] = [ card.code ];
 
-        cards.forEach(card => {
-            // ignore alt-arts
-            if (card.pack_code === 'alt') {
-                return;
-            }
-            // massage card text and flavor-text.
-            if (card.text) {
-                cardTextReplacement.forEach(searchReplace => {
-                    card.text = card.text.replace(searchReplace[0], searchReplace[1]);
-                });
-            }
+    // index cards by name
+    if (! data.indices.names.hasOwnProperty(nameKey)) {
+      data.indices.names[nameKey] = [];
+    }
+    data.indices.names[nameKey].push(card.code);
+  });
 
-            // store card by code.
-            data.cards[card.code] = card;
-            const packKey = makeKeyFromPackCode(card.pack_code);
-            const nameKey = makeKeyFromName(card.title);
+  // make another pass over the names index, and sort the card codes for each title entry.
+  const names = Object.keys(data.indices.names);
+  names.forEach(name => {
+    data.indices.names[name].sort(sortCardCodes);
+  })
+  fs.writeFile(exportFile, JSON.stringify(data), 'utf8', (e) => {
+    if (e) throw e;
+  });
+}
 
-            // index cards by pack
-            if (! data.indices.packs.hasOwnProperty(packKey)) {
-                data.indices.packs[packKey] = {};
-            }
-            data.indices.packs[packKey][nameKey] = [ card.code ];
+(async () => await importCards())()
 
-            // index cards by name
-            if (! data.indices.names.hasOwnProperty(nameKey)) {
-                data.indices.names[nameKey] = [];
-            }
-            data.indices.names[nameKey].push(card.code);
-        });
 
-        // make another pass over the names index, and sort the card codes for each title entry.
-        const names = Object.keys(data.indices.names);
-        names.forEach(name => {
-            data.indices.names[name].sort(sortCardCodes);
-        })
-        fs.writeFile(exportFile, JSON.stringify(data), 'utf8', (e) => {
-            if (e) throw e;
-        });
-    });
-}).on('error', (e) => {
-    console.error(e);
-});
