8c5069ed366cac389cc9d143ac68176d8a6725fb
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-17 11:32:00 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var Discord = require("discord.js");
-const Servo = require('./lib/bot');
-
-if (!process.env.DISCORD_TOKEN) {
-  console.log('Error: Specify DISCORD_TOKEN in environment');
-  process.exit(1);
-}
-
-new Servo(process.env.DISCORD_TOKEN);
